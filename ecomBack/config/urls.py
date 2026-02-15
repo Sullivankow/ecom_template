bd21cc78e0eb351e38b5b17123212d7b21640ea1
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2026-02-15 18:24:00 +0100

    Ajout PATCH user + test postman OK

--- ecomBack/config/urls.py
+++ ecomBack/config/urls.py
@@ -18,10 +18,10 @@ from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
-
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
+
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -31,6 +31,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    
 )
 
 urlpatterns = [
--- ecomBack/users/urls.py
+++ ecomBack/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import RegisterView, LoginView
+from .views import UserSelfUpdateView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
      path('login/', LoginView.as_view(), name='login'),
+     path('me/update/', UserSelfUpdateView.as_view(), name='user-self-update'),
 ]
