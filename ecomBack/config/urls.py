1c74f23f26996a48372b64ec471cc28f8c59c5dc
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2026-02-15 14:14:13 +0100

    Update

--- ecomBack/config/urls.py
+++ ecomBack/config/urls.py
@@ -19,6 +19,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 
@@ -40,4 +41,5 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+        
 ]
